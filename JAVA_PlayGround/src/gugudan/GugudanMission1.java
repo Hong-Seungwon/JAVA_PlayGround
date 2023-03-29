46ae70dd0480c87e29cc6bc31f61220e93d3672e
Hong-Seungwon <woni9774@nave.com>
2023-03-29 18:41:20 +0900

    요구사항1

---
+++ JAVA_PlayGround/src/gugudan/GugudanMission1.java
@@ -0,0 +1,30 @@
+package gugudan;
+
+import java.util.Scanner;
+
+public class GugudanMission1 {
+	
+
+	public static void main(String[] args) {
+		System.out.print("숫자 입력 : ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+    
+		// 세로
+		for(int i = 2; i <= num; i++) {
+			for(int j = 1; j <= num; j++) {
+				System.out.println(i + " * " + j + " = " + i * j);
+			}
+			System.out.println();
+		}
+		System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+		// 가로
+		for(int i = 1; i <= num; i++) {
+			for(int j = 2; j <= num; j++) {
+				System.out.print(j + " * " + i + " = " + i * j + "\t");
+			}
+			System.out.println();
+		}
+		
+	}
+}
