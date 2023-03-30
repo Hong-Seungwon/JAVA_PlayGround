75a6f7fdb37a34fafec303a8cf3a0db53584dcad
Hong-Seungwon <woni9774@nave.com>
2023-03-30 16:16:13 +0900

    반복 입력 가능

--- JAVA_PlayGround/src/calendar/Calendar.java
+++ JAVA_PlayGround/src/calendar/Calendar.java
@@ -53,13 +53,17 @@ public class Calendar {
 		
 			Scanner sc = new Scanner(System.in);
 			Calendar cal = new Calendar();
-			System.out.println("달을 입력하세요");
-			int month = sc.nextInt();
-		
-			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));
 			
-			cal.printSampleCalendar();
+			System.out.println("반복 횟수를 입력하세요.");
+			int repeat = sc.nextInt();
 			
+			for(int i = 0; i < repeat; i++) {				
+				System.out.println("달을 입력하세요");
+				int month = sc.nextInt();
+				System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));
+			}
+//			cal.printSampleCalendar();
+			System.out.println("Bye~");
 			sc.close();
 	}
 }
