339589141c233da56d09190e208dc92d97be0ea0
Hong-Seungwon <woni9774@nave.com>
2023-03-30 17:54:09 +0900

    조건문 생성

--- JAVA_PlayGround/src/calendar/Prompt.java
+++ JAVA_PlayGround/src/calendar/Prompt.java
@@ -29,22 +29,27 @@ public class Prompt {
 		int weekday = 0;
 		
 		while(true) {
-			System.out.println("년도를 입력하세요");
+			System.out.println("년도를 입력하세요(exit : -1)");
 			System.out.print("YEAR> ");
 			year = sc.nextInt();
+			if(year == -1) break;
+			
 			System.out.println("달을 입력하세요");
 			System.out.print("MONTH> ");
 			month = sc.nextInt();
+			if(month < 1 || month > 12) {
+				System.out.println("잘못된 입력입니다.");
+				continue;
+			}
+			 
 			System.out.println("첫째 날의 요일을 입력하세요.(su, mo, tu, we, th, fr, sa)");
 			String str_weekday = sc.next();
 			weekday = parseDay(str_weekday);
-			
-			if(month == -1) {
-				break;
-			}
-			if(month > 12) {
+			if(weekday == parseDay(str_weekday)) {
+				System.out.println("잘못된 입력입니다.");
 				continue;
 			}
+			
 			cal.printCalendar(year, month, weekday);
 		}	
 		System.out.println("Bye~");
