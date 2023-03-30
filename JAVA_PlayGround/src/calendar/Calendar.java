b68aafd2877731a1246770c7413fb559a8470ad6
Hong-Seungwon <woni9774@nave.com>
2023-03-30 17:44:13 +0900

    더 정교한 달력 찍기

--- JAVA_PlayGround/src/calendar/Calendar.java
+++ JAVA_PlayGround/src/calendar/Calendar.java
@@ -1,95 +1,85 @@
 package calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	private static final int[] LEAP_MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public boolean isLeapYear(int year) {
-		if(year % 4 == 0 && (year % 100 != 0 || year % 400 != 0)) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 != 0)) {
 			return true;
-		}else {
-		return false;
+		} else {
+			return false;
 		}
 	}
-	
+
 //	public int maxDaysOfMonth(int month) {
 //		return MAX_DAYS[month -1];
 //	}
-	
+
 	public int getMaxDaysOfMonth(int year, int month) {
-		if(isLeapYear(year)) {
+		if (isLeapYear(year)) {
 			return LEAP_MAX_DAYS[month - 1];
-		}else {
-		return MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
 		}
 	}
 
-	/*
-	 * 월을 입력하면 해당월의 달력을 출력한다. 
-	 * 달력은 모양은 1단계에서 작성한 모양으로 만든다. 
-	 * 1일은 일요일로 정해도 무방하다. 
-	 * -1을 입력받기 전까지 반복 입력받는다. 
-	 * 프롬프트를 출력한다.
-	 */
-	
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("   <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
-		
+
+		// print blank space
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+
 		int maxDay = getMaxDaysOfMonth(year, month);
-		
-		for(int i = 1; i <= maxDay; i++) {
+
+		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
-			if(i % 7 == 0) {
+			if((i + weekday) % 7 == 0) {
 				System.out.println();
 			}
 		}
-		System.out.println();
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12  13 14");
-//		System.out.println(" 15 16 17 18 19 20 21");
-//		System.out.println(" 22 23 24 25 26 27 28");
+		System.out.println("\n");
 	}
-	
+
 	public static void main(String[] args) {
 //		System.out.println("Hello, Calendar");
-		
+
 //		System.out.println(" 일  월 화  수 목  금  토");
 //		System.out.println("---------------------");
 //		System.out.println(" 1  2  3  4  5  6  7");
 //		System.out.println(" 8  9 10 11 12  13 14");
 //		System.out.println(" 15 16 17 18 19 20 21");
 //		System.out.println(" 22 23 24 25 26 27 28");	
-		
-		
-			// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 //			Scanner sc = new Scanner(System.in);
 //			System.out.println("달을 입력하세요");	
 //			int month = sc.nextInt();
-			
+
 //			int[] maxdays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 //			System.out.printf("%d월은 %d일까지 있습니다.", month, maxdays[month - 1]);
 //			sc.close();
-			
+
 //			Scanner sc = new Scanner(System.in);
 //			Calendar cal = new Calendar();
 //			System.out.println("달을 입력하세요");
 //			int month = sc.nextInt();
-			
+
 //			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.maxDaysOfMonth(month));
 //			sc.close();
-		
+
 //			String PROMPT = "cal> ";
 //			Scanner sc = new Scanner(System.in);
 //			Calendar cal = new Calendar();
-			
+
 //			System.out.println("반복 횟수를 입력하세요.");
 //			int repeat = sc.nextInt();
-		
+
 //			for(int i = 0; i < repeat; i++) {				
 //				System.out.println("달을 입력하세요");
 //				System.out.print(PRMOMPT);
@@ -97,9 +87,9 @@ public class Calendar {
 //				System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));
 //			}
 //			cal.printSampleCalendar();
-			
+
 //			int month = 1;
-			
+
 //			while(true) {
 //				System.out.println("달을 입력하세요");
 //				System.out.print(PROMPT);
@@ -107,13 +97,13 @@ public class Calendar {
 //				if(month == -1) {
 //					break;
 //				}
-				
+
 //				if(month > 12) {
 //					continue;
 //				}
 //				System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));
 //			}
-			
+
 //			System.out.println("Bye~");
 //			sc.close();
 	}
--- JAVA_PlayGround/src/calendar/Prompt.java
+++ JAVA_PlayGround/src/calendar/Prompt.java
@@ -2,15 +2,31 @@ package calendar;
 
 import java.util.Scanner;
 
-public class Prompt {
-	private final static String PROMPT="cal> ";
+public class Prompt {	
+	/**
+	 * 
+	 * @param week 요일명
+	 * @return 0 ~ 6 (0 = Sunday, 6 = Saturday)
+	 */
+	public int parseDay(String week) {
+		if(week.equals("su")) return 0;
+		else if(week.equals("mo")) return 1;
+		else if(week.equals("tu")) return 2;
+		else if(week.equals("we")) return 3;
+		else if(week.equals("th")) return 4;
+		else if(week.equals("fr")) return 5;
+		else if(week.equals("sa")) return 6;
+		else
+			return 0;
+	}
 	
 	public void runPrompt() {
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
-		int month = -1;
-		int year = -1;
+		int month = 1;
+		int year = 2023;
+		int weekday = 0;
 		
 		while(true) {
 			System.out.println("년도를 입력하세요");
@@ -19,14 +35,17 @@ public class Prompt {
 			System.out.println("달을 입력하세요");
 			System.out.print("MONTH> ");
 			month = sc.nextInt();
+			System.out.println("첫째 날의 요일을 입력하세요.(su, mo, tu, we, th, fr, sa)");
+			String str_weekday = sc.next();
+			weekday = parseDay(str_weekday);
+			
 			if(month == -1) {
 				break;
 			}
-			
 			if(month > 12) {
 				continue;
 			}
-			cal.printCalendar(year, month);
+			cal.printCalendar(year, month, weekday);
 		}	
 		System.out.println("Bye~");
 		sc.close();
